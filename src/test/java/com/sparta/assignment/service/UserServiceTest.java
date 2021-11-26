80d27f0c9553f0c1df18211ae166b0516e9a3604
Zabee52 <zabeee52@gmail.com>
2021-11-26 23:08:24 +0900

    Testcode modify
    complete case를 형식에 맞게 변경했습니다.

--- src/test/java/com/sparta/assignment/service/UserServiceTest.java
+++ src/test/java/com/sparta/assignment/service/UserServiceTest.java
@@ -24,14 +24,65 @@ class UserServiceTest {
     String nickname;
     String password;
     String passwordCheck;
-    SignUpRequestDto testSignUpRequestDto;
 
     @BeforeEach
     void setup() {
         nickname = "test";
         password = "1234";
         passwordCheck = "1234";
-        testSignUpRequestDto = new SignUpRequestDto(nickname, password, passwordCheck);
+    }
+
+    public String signUp(SignUpRequestDto signUpRequestDto) {
+        // 회원가입 작업
+        // return message
+        String nickname = signUpRequestDto.getNickname();
+        String password = signUpRequestDto.getPassword();
+
+        // 닉네임 중복여부 검사
+        Optional<User> found = userRepository.findByNickname(nickname);
+        if (found.isPresent()) {
+            throw new IllegalArgumentException("중복된 닉네임입니다.");
+        }
+        // 유효성 검사
+        nicknameValidCheck(nickname);
+        passwordValidCheck(password, nickname);
+
+        String passwordCheck = signUpRequestDto.getPasswordCheck();
+        if (!password.equals(passwordCheck)) {
+            throw new IllegalArgumentException("비밀번호와 비밀번호 확인란의 값이 다릅니다.");
+        }
+
+        return "회원가입 완료";
+    }
+
+    private void nicknameValidCheck(String nickname) {
+        // 닉네임은 최소 3자 이상
+        if (nickname.length() >= 3) {
+            // 알파벳 대소문자(a~z, A~Z), 숫자(0~9)로 구성
+            // regexp 사용해도 되지만 알고리즘 문제 풀듯이 풀어봤다.
+            char[] chars = nickname.toCharArray();
+            for (char c : chars) {
+                if (!((c >= 'a' && c <= 'z') ||
+                        (c >= 'A' && c <= 'Z') ||
+                        (c >= '0' && c <= '9'))) {
+                    throw new IllegalArgumentException("닉네임은 대소문자와 숫자로만 구성되어야 합니다.");
+                }
+            }
+        } else {
+            throw new IllegalArgumentException("닉네임은 3자 이상이어야 합니다.");
+        }
+    }
+
+    private void passwordValidCheck(String password, String nickname) {
+        // 비밀번호는 최소 4자 이상
+        if (password.length() >= 4) {
+            // 닉네임과 같은 값이 포함된 경우 회원가입에 실패
+            if (password.contains(nickname)) {
+                throw new IllegalArgumentException("비밀번호에 닉네임이 포함되면 안 됩니다.");
+            }
+        } else {
+            throw new IllegalArgumentException("비밀번호는 4자 이상이어야 합니다.");
+        }
     }
 
     @Test
@@ -41,12 +92,10 @@ class UserServiceTest {
         SignUpRequestDto signUpRequestDto = new SignUpRequestDto(nickname, password, passwordCheck);
 
         // when
-
+        String result = signUp(signUpRequestDto);
 
         // then
-        assertDoesNotThrow(() -> {
-            signUp(signUpRequestDto);
-        });
+        assertEquals("회원가입 완료", result);
     }
 
     @Nested
@@ -155,59 +204,6 @@ class UserServiceTest {
 
             // then
             assertEquals("비밀번호와 비밀번호 확인란의 값이 다릅니다.", exception.getMessage());
-        }
-    }
-
-    public String signUp(SignUpRequestDto signUpRequestDto) {
-        // 회원가입 작업
-        // return message
-        String nickname = signUpRequestDto.getNickname();
-        String password = signUpRequestDto.getPassword();
-
-        // 닉네임 중복여부 검사
-        Optional<User> found = userRepository.findByNickname(nickname);
-        if (found.isPresent()) {
-            throw new IllegalArgumentException("중복된 닉네임입니다.");
-        }
-        // 유효성 검사
-        nicknameValidCheck(nickname);
-        passwordValidCheck(password, nickname);
-
-        String passwordCheck = signUpRequestDto.getPasswordCheck();
-        if (!password.equals(passwordCheck)) {
-            throw new IllegalArgumentException("비밀번호와 비밀번호 확인란의 값이 다릅니다.");
-        }
-
-        return "회원가입 완료";
-    }
-
-    private void nicknameValidCheck(String nickname) {
-        // 닉네임은 최소 3자 이상
-        if (nickname.length() >= 3) {
-            // 알파벳 대소문자(a~z, A~Z), 숫자(0~9)로 구성
-            // regexp 사용해도 되지만 알고리즘 문제 풀듯이 풀어봤다.
-            char[] chars = nickname.toCharArray();
-            for (char c : chars) {
-                if (!((c >= 'a' && c <= 'z') ||
-                        (c >= 'A' && c <= 'Z') ||
-                        (c >= '0' && c <= '9'))) {
-                    throw new IllegalArgumentException("닉네임은 대소문자와 숫자로만 구성되어야 합니다.");
-                }
-            }
-        } else {
-            throw new IllegalArgumentException("닉네임은 3자 이상이어야 합니다.");
-        }
-    }
-
-    private void passwordValidCheck(String password, String nickname) {
-        // 비밀번호는 최소 4자 이상
-        if (password.length() >= 4) {
-            // 닉네임과 같은 값이 포함된 경우 회원가입에 실패
-            if (password.contains(nickname)) {
-                throw new IllegalArgumentException("비밀번호에 닉네임이 포함되면 안 됩니다.");
-            }
-        } else {
-            throw new IllegalArgumentException("비밀번호는 4자 이상이어야 합니다.");
         }
     }
 }
